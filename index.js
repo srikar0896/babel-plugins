d94cd686873dc53034b0e7f91698c124cc278e44
srikar0896 <srikar.0896@gmail.com>
2019-01-20 04:04:03 +0530

    ast must be json.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const run_config = require('./run-config').default;
 const fs = require('fs');
 
 const plugin = require(`./plugins/${run_config.plugin_file_name}`);
+
 const fileLocation = `./code/${run_config.to_be_transformed_file_name}.js`;
 
 const output_file_name = run_config.output_file_name ? run_config.output_file_name : run_config.to_be_transformed_file_name;
@@ -20,5 +21,5 @@ const { code, ast } = babel.transformFileSync(fileLocation, {
 fs.writeFileSync(`./output/transformed-${output_file_name}.js`, code);
 
 if (!!ast) {
-  fs.writeFileSync(`./output/ast-${output_file_name}.js`, JSON.stringify(ast));
+  fs.writeFileSync(`./output/ast-${output_file_name}.json`, JSON.stringify(ast));
 }

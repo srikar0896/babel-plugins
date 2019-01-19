72d27cbccd0402e2488ceb263bfdfedd018d2bc7
srikar0896 <srikar.0896@gmail.com>
2019-01-20 04:19:07 +0530

    Plugin which coverts props with no value to true as default

---
+++ output/transformed-twoPlugins.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const hello = () => {
+  const err = 14;
+  return React.createElement("div", {
+    css: true,
+    css: true
+  }, "Hello world");
+};
--- run-config.js
+++ run-config.js
@@ -1,5 +1,6 @@
 exports.default = {
-  plugin_file_name: 'propsDefaultValueToFalse',
+  plugin_file_names: ['propsDefaultValueToTrue', 'propsDefaultValueToFalse' ],
   to_be_transformed_file_name: 'propsWithNoValue',
   generate_ast: true,
+  output_file_name: 'twoPlugins'
 };

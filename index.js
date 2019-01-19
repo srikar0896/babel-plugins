69414d8049448ca591e08c8dc4010eae22038933
srikar0896 <srikar.0896@gmail.com>
2019-01-20 03:36:01 +0530

    Run plugins using this script and generate output.

---
+++ index.js
@@ -0,0 +1,28 @@
+const babel = require('babel-core');
+const run_config = require('./run-config').default;
+const fs = require('fs');
+
+console.log(run_config);
+
+const plugin = require(`./plugins/${run_config.plugin_file_name}`);
+const fileLocation = `./code/${run_config.to_be_transformed_file_name}.js`;
+
+const output_file_name = run_config.output_file_name ? run_config.output_file_name : run_config.to_be_transformed_file_name;
+
+const { code, ast } = babel.transformFileSync(fileLocation, {
+  plugins: [plugin],
+  ast: run_config.generate_ast ? run_config.generate_ast : false,
+  "presets": [
+    [
+      "@babel/preset-react",
+    ]
+  ]
+});
+
+fs.writeFileSync(`./output/transformed-${output_file_name}.js`, code);
+
+if (!!ast) {
+  fs.writeFileSync(`./output/ast-${output_file_name}.js`, JSON.stringify(ast));
+}
+console.log(ast);
+// console.log(code);
---
+++ run-config.js
@@ -0,0 +1,5 @@
+exports.default = {
+  plugin_file_name: 'propsDefaultValueToFalse',
+  to_be_transformed_file_name: 'propsWithNoValue',
+  generate_ast: true,
+};

4b5ea034f3da128ef5857ded11b12f48613bf596
srikar0896 <srikar.0896@gmail.com>
2019-01-20 16:13:52 +0530

    added warning if plugin is missing.

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const babel = require('babel-core');
 const run_config = require('./run-config').default;
 const fs = require('fs');
+const path=require('path');
 
 const fileLocation = `./code/${run_config.to_be_transformed_file_name}.js`;
 
 const output_file_name = run_config.output_file_name ? run_config.output_file_name : run_config.to_be_transformed_file_name;
 
 const { code, ast } = babel.transformFileSync(fileLocation, {
-  plugins: run_config.plugin_file_names.reduce((acc, plugin_file_name) => [
-    ...acc,
-    require(`./plugins/${plugin_file_name}`)
-  ], []),
+  plugins: run_config.plugin_file_names.reduce((acc, plugin_file_name) => {
+    
+    const plugin = `./plugins/${plugin_file_name}`;
+    
+    if (fs.existsSync(path.join(__dirname, `/plugins/${plugin_file_name}.js`))) {
+      return [
+        ...acc,
+        require(plugin)
+      ]
+    } else {
+      console.warn('\x1b[31m',`plugin - ${plugin_file_name} does not exist.`, '\x1b[0m');
+      return acc;
+    }
+  }, []),
   ast: run_config.generate_ast ? run_config.generate_ast : false,
   "presets": [
     [
@@ -24,3 +35,5 @@ fs.writeFileSync(`./output/transformed-${output_file_name}.js`, code);
 if (!!ast) {
   fs.writeFileSync(`./output/ast-${output_file_name}.json`, JSON.stringify(ast));
 }
+
+console.log('\x1b[32m',`Transformed code saved in  - ${output_file_name}.js`, '\x1b[0m');

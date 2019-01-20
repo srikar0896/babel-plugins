b5998225fa779ae70ecc95bc240eb07de3cfa2b2
srikar0896 <srikar.0896@gmail.com>
2019-01-21 00:42:17 +0530

    lifecycle methods must not be binded

---
+++ constants.js
@@ -0,0 +1,9 @@
+exports.default = {
+  RESERVED_CLASS_METHODS: ['constructor'],
+  LIFE_CYCLE_METHODS: [
+    'componentWillMount', 'componentDidMount',
+    'componentWillReceiveProps', 'shouldComponentUpdate', 'componentWillUpdate', 'componentDidUpdate', 'render',
+    'componentWillUnmount',
+    'componentDidCatch',
+  ]
+};
--- plugins/BindClassMethods.js
+++ plugins/BindClassMethods.js
@@ -1,35 +1,38 @@
 module.exports = ({ types: t }) => {
-  const RESERVED_CLASS_METHODS = ['constructor', 'render'];
+  const { RESERVED_CLASS_METHODS, LIFE_CYCLE_METHODS } = require('../constants').default;
 
   return {
     visitor: {
       ClassMethod(path) {
-        if (!RESERVED_CLASS_METHODS.includes(path.node.key.name)) {
+        if (!RESERVED_CLASS_METHODS.includes(path.node.key.name)
+          && !(LIFE_CYCLE_METHODS.includes(path.node.key.name))) {
+          
           const classMethodName = path.node.key.name;
-          path.parentPath.traverse({
-            ClassMethod(path) {              
-              if (path.node.key.name === 'constructor') {
-                path.get('body').pushContainer('body', t.expressionStatement(
-                  t.assignmentExpression(
-                    '=',
-                    t.memberExpression(t.thisExpression(), t.identifier(classMethodName)),
-                    t.callExpression(
-                      t.memberExpression(
+
+            path.parentPath.traverse({
+              ClassMethod(path) {
+                if (path.node.key.name === 'constructor') {
+                  path.get('body').pushContainer('body', t.expressionStatement(
+                    t.assignmentExpression(
+                      '=',
+                      t.memberExpression(t.thisExpression(), t.identifier(classMethodName)),
+                      t.callExpression(
                         t.memberExpression(
-                          t.thisExpression(),
-                          t.identifier(classMethodName)
+                          t.memberExpression(
+                            t.thisExpression(),
+                            t.identifier(classMethodName)
+                          ),
+                          t.identifier("bind")
                         ),
-                        t.identifier("bind")
-                      ),
-                      [t.thisExpression()]
+                        [t.thisExpression()]
+                      )
                     )
-                  )
-                ));
-              } else {
-                path.skip();
+                  ));
+                } else {
+                  path.skip();
+                }
               }
-            }
-          });
+            });
         }
       }
     },
